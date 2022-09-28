436f2d1db2f6ac144b5570d91e341e911cc02437
Ilya Levshenkov <111687010+ilyalevshenkov@users.noreply.github.com>
2022-09-28 23:12:20 +0300

    Задача 64

---
+++ Ex64/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+Console.WriteLine("Введите значение N : ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string GapNumber(int number)
+{
+    string result = "";
+    if (number == 1)
+    {
+        result = "1";
+    }
+    else
+    {
+        result = GapNumber(number - 1) + "," + $"{number}";
+    }
+    return result;
+}
+
+string result = GapNumber(number);
+Console.WriteLine($"N = {number}. Промежуток натуральных чисел: {result}");
